1c9eb37de1602b46a2e5cd40187b911a381d6f38
Lynx-Lynx <delinne.weird@gmail.com>
2021-09-07 16:15:04 +0300

    changed UI

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-let button = document.getElementById("enter");
-let input = document.getElementById("userinput");
-let ul = document.querySelector("ul");
-let li = document.getElementsByTagName("li");
+const enterButton = document.getElementById("enter");
+const resetButton = document.getElementById("reset");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+const li = document.getElementsByTagName("li");
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 		li.appendChild(document.createTextNode(input.value));
 		ul.appendChild(li);
 		input.value = "";
 		li.className = "undone";
 		li.onclick = markDone;
-	  let listbutton = document.createElement("button");
-		listbutton.appendChild(document.createTextNode("Delete"));
-		li.appendChild(listbutton);
-		listbutton.className = "listbutton";
-		listbutton.onclick=removeItem;
+	let removebutton = document.createElement("div");
+		li.appendChild(removebutton);
+		removebutton.className = "removebutton";
+		removebutton.onclick = removeItem;
 }
 
 function markDone() {
-		this.classList.toggle("done");
-		}
+	this.classList.toggle("done");
+}
 
 function removeItem() {
 	this.parentNode.remove();
-		}
+}
+
+function removeAllItems() {
+	ul.remove();
+}
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
@@ -41,6 +45,8 @@ function addListAfterKeyPress(event) {
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+enterButton.addEventListener("click", addListAfterClick);
+
+resetButton.addEventListener("click", removeAllItems);
 
 input.addEventListener("keypress", addListAfterKeyPress);
